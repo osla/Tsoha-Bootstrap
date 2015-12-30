7477308254578f306dddb7e68633e047567a7c1a
Mikko Järvi <mikko@Mikko-MacBook-Air.local>
2015-12-31 00:08:15 +0200

    Testidata lisätty

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,6 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Opiskelija (sposti) VALUES ('mikko.mallikas@helsinki.fi');
+INSERT INTO Opiskelija (sposti) VALUES ('timo.mallikas@helsinki.fi');
+INSERT INTO Laitos (laitoksenNimi) VALUES ('tietojenkäsittelytieteen laitos');
+INSERT INTO Kayttaja (kayttajanNimi, kayttajanSposti, salasana) VALUES ('Opettaja, Olli', 'olli.opettaja@helsinki.fi', 'olli123');
+INSERT INTO Kurssi (kurssinNimi) VALUES ('Ohjelmistotuotanto');
